2de1412e57fe30085161ab7ca3fc138a8cb6a327
Slawomir Niemiec <slawekniemiec@gmail.com>
2020-03-06 08:58:59 +0000

    Updates

---
+++ precache-manifest.0a16f91cba3fc1721d30d7b2ae4dda3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f95d1bca1990c6ca03aa3e77cb3d0b4c",
+    "url": "/Web-Speed-App/index.html"
+  },
+  {
+    "revision": "90712cd25b4598229111",
+    "url": "/Web-Speed-App/static/css/main.1bb4f880.chunk.css"
+  },
+  {
+    "revision": "7d500ff390cdd9bde12c",
+    "url": "/Web-Speed-App/static/js/2.aebf2ef1.chunk.js"
+  },
+  {
+    "revision": "f007e5fde3f2c3983c1b0d874ba90212",
+    "url": "/Web-Speed-App/static/js/2.aebf2ef1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "90712cd25b4598229111",
+    "url": "/Web-Speed-App/static/js/main.b4d9cac8.chunk.js"
+  },
+  {
+    "revision": "a1cb17cb6d56e8c7780b",
+    "url": "/Web-Speed-App/static/js/runtime-main.a8832365.js"
+  }
+]);
